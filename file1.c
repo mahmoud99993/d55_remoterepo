35d44f4314a2a8b928e949b40cce4194910d65df
mahmoud99993 <engmahmoudhamdy93@gmail.com>
2023-09-08 21:00:02 +0200

    ver1

---
+++ file1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "file1.h"
+
+void printmyname(void)
+{
+printf("MAHMOUD HAMDY");
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "file1"
+
+int main 
+{
+
+return 0;
+}
